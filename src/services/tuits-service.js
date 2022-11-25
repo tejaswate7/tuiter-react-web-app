b33c85853d46af5ad14c014e4fecc74019ae484e
Tejas Wate <tejaswate@gmail.com>
2022-11-24 21:40:07 -0500

    Added environment variable for a9 -> render deploy

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // const TUITS_API = "http://localhost:4000/api/tuits"
 // const TUITS_API = 'https://tuiter-node-server-app-tejas.herokuapp.com/api/tuits';
-const API_BASE = process.env.REACT_APP_API_BASE;
+const API_BASE = process.env.REACT_APP_API_BASE_WITH_MONGO;
 const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
